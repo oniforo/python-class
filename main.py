d87b1aee37878d8560e0f37957ff497aad68b83e
Gabriel Orofino <g.orofino@protonmail.com>
2022-08-09 16:53:04 -0300

    Finished error handling and created unit test file.

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 def main():
     return
 
-def calc_area(length, height):
-    try:
-        area = int(length) * int(height)
-    except ValueError:
-        print("Inputs must be numbers.")
-        return
-    return area
-
 if __name__ == "__main__":
-    length = input("Insert length: ")
-    height = input("Insert height: ")
-    result = calc_area(length, height)
-    if result: print(f"The area is {result}.")
+    main()
