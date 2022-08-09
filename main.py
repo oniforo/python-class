7b4d9fc70fd72263b59b5d83a516bde7b3398a9a
Gabriel Orofino <g.orofino@protonmail.com>
2022-08-09 16:20:12 -0300

    Added error handling.

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ def main():
     return
 
 def calc_area(length, height):
-    return int(length) * int(height)
+    try:
+        area = int(length) * int(height)
+    except ValueError:
+        print("Inputs must be numbers.")
+        return
+    return area
 
 if __name__ == "__main__":
     length = input("Insert length: ")
     height = input("Insert height: ")
     result = calc_area(length, height)
-    print(f"The area is {result}.")
+    if result: print(f"The area is {result}.")
