e4712b77470fcd769dcaa78c48838b366945c1db
Gabriel Orofino <g.orofino@protonmail.com>
2022-08-09 16:13:46 -0300

    Created a function to calculate area.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def main():
     return
 
+def calc_area(length, height):
+    return int(length) * int(height)
+
 if __name__ == "__main__":
-    main()
+    length = input("Insert length: ")
+    height = input("Insert height: ")
+    result = calc_area(length, height)
+    print(f"The area is {result}.")
