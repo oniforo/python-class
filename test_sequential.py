478f0476c12649164925743dfd454cf8bd444bb6
Gabriel Orofino <g.orofino@protonmail.com>
2022-08-11 19:33:29 -0300

    Created unit testing module for sequential.py

---
+++ test_sequential.py
@@ -0,0 +1,11 @@
+import unittest
+import sequential
+
+class TestSequential(unittest.TestCase):
+
+    def test_calc_download_eta(self):
+        return
+
+
+if __name__ == "__main__":
+    unittest.main()
